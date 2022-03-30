0968e96ee4559f832f8dbe26e0fa41b297ce145a
Alina Petrenko <alina.petrenko@globallogic.com>
2022-03-30 17:06:56 +0300

    init

--- FifthTask/TreeSort.cs
+++ FifthTask/TreeSort.cs
@@ -12,32 +12,22 @@ namespace FifthTask
         private Stopwatch _stopwatch = new Stopwatch();
 
         /// <summary>
-        /// Calls methods for sorting array and calculating elapsed time
+        /// Sorts <paramref name="array"/>, adds <paramref name="array"/> values ​​to methods for sorting 
         /// </summary>
         /// <param name="array">One-dimensional array with random numbers</param>
         /// <returns>Returns spent time for calculations</returns>
         public TimeSpan GetStartTreeSort(int[] array)
         {
             _stopwatch.Start();
-            Sort(array);
-            _stopwatch.Stop();
-            _timeSpan = _stopwatch.Elapsed;
-            return _timeSpan;
-        }
-
-        /// <summary>
-        /// Sorts <paramref name="array"/>, adds <paramref name="array"/> values ​​to methods for sorting 
-        /// </summary>
-        /// <param name="array">One-dimensional array with random numbers</param>
-        /// <returns>Returns a method call to traverse the tree</returns>
-        private int[] Sort(int[] array)
-        {
             var treeNode = new Node(array[0]);
             for (int i = 1; i < array.Length; i++)
             {
                 treeNode.Add(new Node(array[i]));
             }
-            return treeNode.Traverse();
+            treeNode.Traverse();
+            _stopwatch.Stop();
+            _timeSpan = _stopwatch.Elapsed;
+            return _timeSpan;
         }
     }
 }
