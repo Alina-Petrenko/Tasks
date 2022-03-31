c134eee5c6f8e0104172a019b2fa863f9cb39047
Alina Petrenko <alina.petrenko@globallogic.com>
2022-03-31 13:11:16 +0300

    init

--- SixthTask/Program.cs
+++ SixthTask/Program.cs
@@ -1,7 +1,13 @@
 ﻿namespace SixthTask
 {
+    /// <summary>
+    /// Starts the project
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Entry point into application
+        /// </summary>
         static void Main()
         {
             CallManager callManager = new CallManager();
